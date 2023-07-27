970404c54faf09b1fd963a8010c644c66906c9eb
david0411 <tlyeyd@gmail.com>
2023-07-27 12:59:19 +0800

    clean code

--- src/Config/EnvConfig.ts
+++ src/Config/EnvConfig.ts
@@ -2,7 +2,7 @@ import DevConfig from "./DevConfig.ts";
 import ProdConfig from "./ProdConfig.ts";
 
 export default function EnvConfig() {
-    if(process.env.NODE_ENV || process.env.NODE_ENV ==="development")   {
+    if(!process.env.NODE_ENV || process.env.NODE_ENV ==="development")   {
         return DevConfig;
     }   else    {
         return ProdConfig;
